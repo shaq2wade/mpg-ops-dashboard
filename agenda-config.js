b8309782d781118b946c491a0b18d60a7ee50b06
Marty <153335148+shaq2wade@users.noreply.github.com>
2026-07-07 06:17:26 +1200

    Add Outlook digest storage config

--- agenda-config.js
+++ agenda-config.js
@@ -2,6 +2,7 @@ window.MPG_AGENDA_CONFIG = {
   dashboardStorageKey: "mpg_ops_dashboard_data_v4",
   calendarFeedStorageKey: "mpg_agenda_calendar_feed_v1",
   calendarTextStorageKey: "mpg_agenda_calendar_text_v1",
+  outlookDigestStorageKey: "mpg_agenda_outlook_digest_v1",
   lockStorageKey: "mpg_agenda_lock_v1",
   priorityLimit: 8
 };
