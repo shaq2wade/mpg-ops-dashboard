3054b25924596b3a9f5b9780a1f19f9534a5f8bf
Marty <153335148+shaq2wade@users.noreply.github.com>
2026-07-06 16:07:42 +1200

    Add Outlook agenda config

---
+++ agenda-config.js
@@ -0,0 +1,9 @@
+window.MPG_AGENDA_CONFIG = {
+  clientId: "",
+  tenantId: "organizations",
+  redirectUri: "https://shaq2wade.github.io/mpg-ops-dashboard/agenda.html",
+  scopes: ["User.Read", "Calendars.Read"],
+  graphTimeZone: "New Zealand Standard Time",
+  dashboardStorageKey: "mpg_ops_dashboard_data_v4",
+  priorityLimit: 8
+};
